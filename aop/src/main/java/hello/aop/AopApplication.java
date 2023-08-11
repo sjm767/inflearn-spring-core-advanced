5c91d7911177d07a3fefda1517b82f4bc25d1bac
jaeshim <jaeshim@gmarket.com>
2023-08-11 15:06:03 +0900

    20230811

---
+++ aop/src/main/java/hello/aop/AopApplication.java
@@ -0,0 +1,14 @@
+package hello.aop;
+
+import hello.aop.order.aop.AspectV1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+public class AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApplication.class, args);
+	}
+}
---
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,51 @@
+package hello.aop;
+
+import static org.assertj.core.api.Assertions.*;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+public class AopTest {
+
+  @Autowired
+  OrderService orderService;
+
+  @Autowired
+  OrderRepository orderRepository;
+
+  @Test
+  void aopInfo() {
+    log.info("isAopProxy, orderSerivce={}", AopUtils.isAopProxy(orderService));
+    log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+  }
+
+  @Test
+  void success(){
+    orderService.orderItem("itemA");
+
+  }
+  @Test
+  void exception(){
+    assertThatThrownBy(() -> orderService.orderItem("ex")).isInstanceOf(
+        IllegalStateException.class);
+  }
+
+}
